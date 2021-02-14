d000a4894e58e3437b4137167b2d30c3971e6e80
Mensur Mandzuka <mensur.mandzuka@gmail.com>
2021-02-14 20:45:16 +0100

    error response is implementing serializable

--- src/main/java/com/skf/messaging/controller/response/ApiErrorResponse.java
+++ src/main/java/com/skf/messaging/controller/response/ApiErrorResponse.java
@@ -3,8 +3,12 @@ package com.skf.messaging.controller.response;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import java.io.Serializable;
+
 @Data
-public class ApiErrorResponse {
+public class ApiErrorResponse implements Serializable {
+
+    private static final long serialVersionUID = -8201938846322366437L;
 
     private final HttpStatus status;
     private final String message;
